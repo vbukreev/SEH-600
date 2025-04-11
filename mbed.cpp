f0e94dd6d9461573c3913556f9800fc5a7b90f28
Victoria <vbukreev@myseneca.ca>
2025-04-10 23:07:07 -0400

    Changes to mbed code

--- mbed.cpp
+++ mbed.cpp
@@ -1,133 +1,77 @@
 #include "mbed.h"
-#include "rtos.h"
-#include "FXOS8700Q.h"
-#include "LCDi2c.h"  // Make sure this library is included in your project
+#include "LCDi2c.h"
 
-// Accelerometer (FXOS8700Q) over I2C
-I2C i2cAcc(PTD9, PTD8);
-FXOS8700QAccelerometer acc(i2cAcc, FXOS8700CQ_SLAVE_ADDR1);
+// UART from Unity
+BufferedSerial pc(USBTX, USBRX, 9600);  // K66 USB virtual COM
 
-// Serial to Unity (USB)
-UnbufferedSerial pc(USBTX, USBRX, 9600);  // USB serial for Unity
+// Motor control pins - H-bridge
+DigitalOut in1(PTD0);     // IN1
+DigitalOut in2(PTB19);    // IN2
 
-// Serial from Unity AI (UART1)
-UnbufferedSerial aiUART(PTC14, PTC15, 9600);  // RX, TX from AI
+// I2C LCD
+LCDi2c lcd(PTC11, PTC10, LCD16x2, 0x27);  // SDA, SCL, Type, Address
 
-// LCD over I2C
-LCDi2c lcd(PTC11, PTC10, LCD16x2, 0x27); // SDA, SCL, Type, Address
+char recv_char;
+int turbulence_level = 0;
 
-// Propeller via transistor
-DigitalOut motorControl(PTD0);
-
-// Shared state
-char movement = '\0';
-int aiPrediction = -1;
-Mutex serialMutex;
-
-void ReadAccelerometer()
-{
-    motion_data_units_t acc_data;
-
-    while (true)
-    {
-        acc.getAxis(acc_data);
-        float ax = acc_data.x;
-        float ay = acc_data.y;
-
-        serialMutex.lock();
-        movement = '\0';
-        if (ax < -0.3) movement = 'L';
-        else if (ax > 0.3) movement = 'R';
-        else if (ay < -0.3) movement = 'F';
-        else if (ay > 0.3) movement = 'B';
-        serialMutex.unlock();
-
-        ThisThread::sleep_for(250ms);
-    }
+void lcd_write_line(int row, const char* text) {
+    lcd.locate(0, row);
+    lcd.puts(text);
 }
 
-void SendToUnity()
-{
-    while (true)
-    {
-        serialMutex.lock();
-        if (movement != '\0')
-        {
-            pc.write(&movement, 1);
-            pc.write("\n", 1);
-            movement = '\0';
-        }
-        serialMutex.unlock();
-        ThisThread::sleep_for(300ms);
-    }
-}
-
-void ReceiveFromAI()
-{
-    char c;
-    static char buffer[4];
-    static int index = 0;
-
-    while (true)
-    {
-        if (aiUART.readable())
-        {
-            aiUART.read(&c, 1);
-            if (c == '\n' || c == '\r')
-            {
-                buffer[index] = '\0';
-                aiPrediction = atoi(buffer);
-                index = 0;
-
-                lcd.cls();
-                lcd.locate(0, 0);
-                switch (aiPrediction)
-                {
-                    case 0: lcd.printf("Turb: Calm"); break;
-                    case 1: lcd.printf("Turb: Mild"); break;
-                    case 2: lcd.printf("Turb: Mod"); break;
-                    case 3: lcd.printf("Turb: Severe!"); break;
-                    default: lcd.printf("Turb: Unknown"); break;
-                }
-            }
-            else if (index < 3)
-            {
-                buffer[index++] = c;
-            }
-        }
-        ThisThread::sleep_for(100ms);
-    }
-}
-
-void ControlPropeller()
-{
-    while (true)
-    {
-        
-        if (aiPrediction == 3) {
-            motorControl = 0; 
-        } else {
-            motorControl = 1;
-        }
-    }
-}
-
-int main()
-{
-    acc.enable();
+void control_motor(int level) {
     lcd.cls();
-    lcd.locate(0, 0);
-    lcd.printf("System Booting...");
-    motorControl = 1;
 
-    Thread t1, t2, t3, t4;
-    t1.start(ReadAccelerometer);
-    t2.start(SendToUnity);
-    t3.start(ReceiveFromAI);
-    t4.start(ControlPropeller);
+    // Show turbulence level
+    char level_str[16];
+    snprintf(level_str, sizeof(level_str), "Turbulence: %d", level);
+    lcd_write_line(0, level_str);
 
-    while (true)
-    {
-        ThisThread::sleep_for(1000ms);  // Keep main thread alive
+    if (level == 1) {
+        // Forward spin
+        lcd_write_line(1, "Fan: FORWARD");
+        in1 = 1;
+        in2 = 0;
+    }
+    else if (level == 2) {
+        // Reverse spin
+        lcd_write_line(1, "Fan: REVERSE");
+        in1 = 0;
+        in2 = 1;
+    }
+    else {
+        // Stop
+        lcd_write_line(1, "Fan: OFF");
+        in1 = 0;
+        in2 = 0;
+    }
+}
+
+int main() {
+    pc.set_format(8, BufferedSerial::None, 1);
+    lcd.cls();
+    lcd_write_line(0, "Awaiting Unity");
+    lcd_write_line(1, "Turbulence lvl");
+
+    while (true) {
+        if (pc.readable()) {
+            pc.read(&recv_char, 1);
+
+            if (recv_char >= '0' && recv_char <= '3') {
+                turbulence_level = recv_char - '0';
+                control_motor(turbulence_level);
+            }
+            else {
+                lcd.cls();
+                lcd_write_line(0, "Invalid input:");
+                char buf[16];
+                snprintf(buf, sizeof(buf), "Char: %c", recv_char);
+                lcd_write_line(1, buf);
+                in1 = 0;
+                in2 = 0;
+            }
+        }
+
+        thread_sleep_for(100);
     }
 }
